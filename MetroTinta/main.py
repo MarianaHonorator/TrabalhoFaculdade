6a2ac4c78f9235fc11695815f7ee2587c8cb6465
MarianaHonorator <92828222+MarianaHonorator@users.noreply.github.com>
2022-10-01 14:41:54 -0300

    Adicionando Comentários

--- MetroTinta/main.py
+++ MetroTinta/main.py
@@ -1,21 +1,23 @@
 import math
 
 med = float(input("Qual o tamanho em metros quadrados da área ?"))
-
+#litros necessarios: 
 litros = med/6
+#10% de folga na mistura:
 litros_folga = litros * 1.1
 latas = litros/18
 galao = litros/3.6
 
+#latas:
 if latas % 18 != 0:
     latas +=1
     preco_lata = math.floor(latas) * 80
-
+#galões:
 if galao % 3.6 != 0:
     galao += 1
     preco_galao = math.floor(galao) * 25
 
-
+#mistura:
 mistura_lata = int(litros_folga/18)
 mistura_galao = int((litros_folga - (mistura_lata * 18)) / 3.6)
 
