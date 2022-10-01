41ec1c638ad749758e5d5e927379cd786c5ed34b
MarianaHonorator <92828222+MarianaHonorator@users.noreply.github.com>
2022-10-01 14:45:11 -0300

    Adicionando Comentários

--- PeixeMulta/main.py
+++ PeixeMulta/main.py
@@ -1,14 +1,14 @@
 
 peso = float(input("Qual o peso do peixe ?"))
 
-
+#caso peso passe de 50kg:
 if peso > 50:
     excesso = peso - 50
-
+#caso não passe:
 else:
     excesso = 0
 
-
+#multa referente ao excesso
 multa = excesso * 4
 
 print(f'QUILO EXCEDENTE: {excesso} Kg')
